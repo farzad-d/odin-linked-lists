df8c517aaa026bf04ab9223ec134911d5c154f88
farzad-d <fzddnp@example.com>
2025-11-21 09:47:39 +0330

    Create Node and LinkedList classes

---
+++ linked-list.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value = null) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.node = null;
+  }
+}
+
+export default LinkedList;
